621a64ff00eea3e3fd505b73ffc09c6c970a5113
EdwinDMendezR <61098711+EdwinDMendezR@users.noreply.github.com>
2023-02-21 08:39:51 -0500

    java-11: HTTP client standard

---
+++ src/test/java/http/client/standard/HttpClientStandarTest.java
@@ -0,0 +1,28 @@
+package http.client.standard;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HttpClientStandarTest {
+
+    @Test
+    public void methodTest() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://jsonplaceholder.typicode.com/posts/1"))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+    }
+
+}
