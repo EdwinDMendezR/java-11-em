e732dc132e0ceefbbab1847630a803b38bdde415
EdwinDMendezR <61098711+EdwinDMendezR@users.noreply.github.com>
2023-02-22 15:20:44 -0500

    java-11: Unicode 10

---
+++ src/test/java/unicode/UnicodeTest.java
@@ -0,0 +1,14 @@
+package unicode;
+
+import org.junit.Test;
+
+public class UnicodeTest {
+
+    @Test
+    public void methodTest() {
+        String frozenFace = "\uD83E\uDD76";
+        System.out.println("*******************************");
+        System.out.println(frozenFace);
+    }
+
+}
