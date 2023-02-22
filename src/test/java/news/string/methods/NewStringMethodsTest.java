30ec6a79d78b36f0ee5d2664e25dc0e4ac548812
EdwinDMendezR <61098711+EdwinDMendezR@users.noreply.github.com>
2023-02-22 17:38:32 -0500

    java-11: Repeat String

---
+++ src/test/java/news/string/methods/NewStringMethodsTest.java
@@ -0,0 +1,14 @@
+package news.string.methods;
+
+import org.junit.Test;
+
+public class NewStringMethodsTest {
+
+    @Test
+    public void methodTest() {
+        String repeat = "repeat\n";
+        System.out.println(repeat.repeat(4));
+    }
+
+
+}
