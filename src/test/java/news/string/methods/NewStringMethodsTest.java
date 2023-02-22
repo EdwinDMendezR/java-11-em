5fab545d5b049f5624c35c6365a9e9b8622e7818
EdwinDMendezR <61098711+EdwinDMendezR@users.noreply.github.com>
2023-02-22 17:42:50 -0500

    java-11: is Blank

--- src/test/java/news/string/methods/NewStringMethodsTest.java
+++ src/test/java/news/string/methods/NewStringMethodsTest.java
@@ -1,13 +1,21 @@
 package news.string.methods;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class NewStringMethodsTest {
 
     @Test
-    public void methodTest() {
+    public void repaatTest() {
         String repeat = "repeat\n";
         System.out.println(repeat.repeat(4));
+    }
+
+    @Test
+    public void methodTest() {
+        String string = "   ";
+        Assert.assertTrue(string.isBlank());
+        Assert.assertFalse(string.isEmpty());
     }
 
 
